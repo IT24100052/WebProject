987957820bf8c3b37f9ae53c8fded06b64b1806d
Afathi_IT24100052 <IT24100052@my.sliit.lk>
2025-10-04 11:52:43 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/controllers/user/UserDeleteServlet.java
@@ -0,0 +1,30 @@
+package com.example.demo.controllers.user;
+import com.example.demo.models.User;
+import com.example.demo.services.ReviewManager;
+import com.example.demo.services.TutorsManager;
+import com.example.demo.services.UserManager;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebServlet(name = "UserDeleteServlet", value = "/delete-user") 
+public class UserDeleteServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        UserManager.readUsers();
+
+        String userIDStr = request.getParameter("user-id");
+
+        int id = Integer.parseInt(userIDStr);  // get the user ID from the request
+
+        UserManager.removeUser(id);
+
+        response.sendRedirect("pages/user/admin-view.jsp"); // After deleting go back to admin user management page
+    }
+}
